2d6f025d51e1f91e2aec1b6616a8d81854ede6a8
James St-Pierre <James@Jamess-iMac.local>
2016-11-29 23:53:48 -0500

    beautified

--- index.js
+++ index.js
@@ -1,158 +1,155 @@
 #!/usr/bin/env node
+
 var program = require('commander');
-const net   = require('net');
+const net = require('net');
 const yargs = require('yargs');
-var http    = require('http');
-var io      = require('socket.io-client');
-var dgram   = require('dgram');
-var ip      = require('ip');
+var http = require('http');
+var io = require('socket.io-client');
+var dgram = require('dgram');
+var ip = require('ip');
 
 
 //use --help to get help
 
 program
-  .version('0.0.1')
-  .command('get <url>')
-	.description('Get executes a HTTP GET request for a given URL.')
-	.option("-v, --v", "Prints the detail of the response such as protocol, status, and headers.")
-  .option("-h, --h", "key:value Associates headers to HTTP Request with the format key:value.")
-  .action(function(url, options) {
-		//contents go here
-    console.log("in get function");
-    // console.log(program.commands[0].v);
-    // console.log(program.commands[0].h);
-    var PORT = 3000;
-    var HOST = '127.0.0.1';
-    // Add a connect listener
+    .version('0.0.1')
+    .command('get <url>')
+    .description('Get executes a HTTP GET request for a given URL.')
+    .option("-v, --v", "Prints the detail of the response such as protocol, status, and headers.")
+    .option("-h, --h", "key:value Associates headers to HTTP Request with the format key:value.")
+    .action(function(url, options) {
+        //contents go here
+        console.log("in get function");
+        // console.log(program.commands[0].v);
+        // console.log(program.commands[0].h);
+        var PORT = 3000;
+        var HOST = '127.0.0.1';
+        // Add a connect listener
 
-    var buf;
-    var urlDict = {
-      type: "get",
-      tempUrl : url,
-      v : program.commands[0].v,
-      h : program.commands[0].h
-    }
-
-    //syn 0, syn-ack 1, ack 2, nack 3, data 4
-
-    //Start handshake
-
-
-    definePacket(0, 1, null);
-    // Define an IP header pattern using a joined array to explode the pattern.
-    var client = dgram.createSocket('udp4');
-	
-	//  timeout 
-    client.send(buf, PORT, HOST, function(err, bytes) {
-        if (err) throw err;
-        console.log('UDP SYN message sent to ' + HOST +':'+ PORT);
-        // client.close();
-    });
-
-    client.on('message', function (message, remote) {
-        buf = Buffer.from(message);
-        var type = buf.readInt8(0);
-        var seqNum = buf.readInt16BE(1);
-        var address = ip.toString(buf, 5, 4);
-
-        if(type == 1){ // if syn-ack
-          buf.writeInt8(2,0);
-          const length = Buffer.byteLength(JSON.stringify(urlDict), 'utf8')
-          const buffer2 = Buffer.allocUnsafe(length)
-          const newBuffer = Buffer.concat([buf, buffer2], length+11);
-          newBuffer.writeInt8(2,0);
-          newBuffer.write(JSON.stringify(urlDict), 11);
-		
-		  // timeout 
-          client.send(newBuffer, PORT, HOST, function(err, bytes) {
-              if (err) throw err;
-              console.log('UDP ACK message sent to ' + HOST +':'+ PORT);
-              // client.close();
-          });
+        var buf;
+        var urlDict = {
+            type: "get",
+            tempUrl: url,
+            v: program.commands[0].v,
+            h: program.commands[0].h
         }
-        else if(type == 2){
 
-        }
-        else if(type == 3){
+        //syn 0, syn-ack 1, ack 2, nack 3, data 4
 
+        //Start handshake
+
+
+        definePacket(0, 1, null);
+        // Define an IP header pattern using a joined array to explode the pattern.
+        var client = dgram.createSocket('udp4');
+
+        //  timeout
+        client.send(buf, PORT, HOST, function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP SYN message sent to ' + HOST + ':' + PORT);
+            // client.close();
+        });
+
+        client.on('message', function(message, remote) {
+            buf = Buffer.from(message);
+            var type = buf.readInt8(0);
+            var seqNum = buf.readInt16BE(1);
+            var address = ip.toString(buf, 5, 4);
+
+            if (type == 1) { // if syn-ack
+                buf.writeInt8(2, 0);
+                const length = Buffer.byteLength(JSON.stringify(urlDict), 'utf8')
+                const buffer2 = Buffer.allocUnsafe(length)
+                const newBuffer = Buffer.concat([buf, buffer2], length + 11);
+                newBuffer.writeInt8(2, 0);
+                newBuffer.write(JSON.stringify(urlDict), 11);
+
+                // timeout
+                client.send(newBuffer, PORT, HOST, function(err, bytes) {
+                    if (err) throw err;
+                    console.log('UDP ACK message sent to ' + HOST + ':' + PORT);
+                    // client.close();
+                });
+            } else if (type == 2) {
+
+            } else if (type == 3) {
+
+            } else if (type == 4) {
+                var body = JSON.parse(buf.toString('utf8', 11));
+                client.close();
+                // output dict.
+                console.log('Status: ' + body.status);
+                console.log('Body: ' + body.body);
+                console.log('Args: ');
+                //console.log(JSON.stringify(body["args"]));
+                for (var key in body["args"]) {
+                    console.log(key + " : " + body["args"][key]);
+                }
+                console.log('Headers : ');
+                for (var key in body["headers"]) {
+                    console.log(key + " : " + body["headers"][key]);
+                }
+
+            }
+
+
+        })
+
+
+        function definePacket(type, seq, body) {
+            var length;
+            if (body != null) {
+                length = Buffer.byteLength(JSON.stringify(body), 'utf8')
+            } else {
+                length = 0
+            }
+            buf = Buffer.allocUnsafe(length + 11)
+            buf.writeInt8(type, 0);
+            buf.writeInt16BE(seq, 1);
+            ip.toBuffer('127.0.0.1', buf, 5);
+            buf.writeInt16BE(8007, 9);
+            if (body != null) {
+                buf.write(JSON.stringify(body), 11);
+            }
         }
-        else if(type == 4){
-          var body = JSON.parse(buf.toString('utf8', 11));
-          client.close();
-		  // output dict.
-          console.log('Status: ' + body.status);
-		  console.log('Body: ' + body.body);
-		  console.log('Args: ');
-				//console.log(JSON.stringify(body["args"]));
-				for (var key in body["args"]) {
-					console.log(key + " : " + body["args"][key]);
-				}
-		  console.log('Headers : ');
-				for (var key in body["headers"]) {
-					console.log(key + " : " + body["headers"][key]);
-				}
-		  
-		  }
 
 
     })
 
-
-    function definePacket(type, seq, body){
-      var length;
-      if(body!=null){
-        length = Buffer.byteLength(JSON.stringify(body), 'utf8')
-      }
-      else{
-        length = 0
-      }
-      buf = Buffer.allocUnsafe(length+11)
-      buf.writeInt8(type, 0);
-      buf.writeInt16BE(seq, 1);
-      ip.toBuffer('127.0.0.1', buf, 5);
-      buf.writeInt16BE(8007, 9);
-      if(body!=null){
-        buf.write(JSON.stringify(body), 11);
-      }
-    }
-
-
-	})
-
 program
-  .command('post <url>')
-  .description('Post executes a HTTP POST request for a given URL with inline data or from file.')
-  .option("-v, --v", "Prints the detail of the response such as protocol, status, and headers.")
-  .option("-h, --h", "key:value Associates headers to HTTP Request with the format key:value.")
-  .option("-d, --string <string>", "Associates an inline data to the body HTTP POST request.")
-  .option("-f --file <file>","Associates the content of a file to the body HTTP POST request.")
-  .option("-o --open <open>","Creates a file with the body of the response to the specified file")
-  .action(function(url, options) {
-		//contents go here
-    console.log("in post function");
-    // console.log(program.commands[0].v);
-    // console.log(program.commands[0].h);
-    var socket = io("http://localhost:3000");
-    // Add a connect listener
-    socket.on('connect',function() {
-       console.log('Client has connected to the server!');
-    });
+    .command('post <url>')
+    .description('Post executes a HTTP POST request for a given URL with inline data or from file.')
+    .option("-v, --v", "Prints the detail of the response such as protocol, status, and headers.")
+    .option("-h, --h", "key:value Associates headers to HTTP Request with the format key:value.")
+    .option("-d, --string <string>", "Associates an inline data to the body HTTP POST request.")
+    .option("-f --file <file>", "Associates the content of a file to the body HTTP POST request.")
+    .option("-o --open <open>", "Creates a file with the body of the response to the specified file")
+    .action(function(url, options) {
+        //contents go here
+        console.log("in post function");
+        // console.log(program.commands[0].v);
+        // console.log(program.commands[0].h);
+        var socket = io("http://localhost:3000");
+        // Add a connect listener
+        socket.on('connect', function() {
+            console.log('Client has connected to the server!');
+        });
 
-    var urlDict = {
-      type: "post",
-      tempUrl : url,
-      v : program.commands[1].v,
-      h : program.commands[1].h,
-      string : program.commands[1].string,
-      file: program.commands[1].file,
-      o : program.commands[1].open,
-      filename : program.commands[1].open
-    }
-    console.log(program.commands[1].open);
+        var urlDict = {
+            type: "post",
+            tempUrl: url,
+            v: program.commands[1].v,
+            h: program.commands[1].h,
+            string: program.commands[1].string,
+            file: program.commands[1].file,
+            o: program.commands[1].open,
+            filename: program.commands[1].open
+        }
+        console.log(program.commands[1].open);
 
-    socket.emit('post', urlDict);
-    socket.on('returnValue', function (data) {
-      console.log(data);
-    });
-	})
+        socket.emit('post', urlDict);
+        socket.on('returnValue', function(data) {
+            console.log(data);
+        });
+    })
 program.parse(process.argv);
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
-var app     = require('http').createServer()
-var io      = require('socket.io')(app);
-var fs      = require('fs');
+var app = require('http').createServer()
+var io = require('socket.io')(app);
+var fs = require('fs');
 var request = require('request');
-var url     = require('url');
-var fs      = require('fs');
-var dgram   = require('dgram');
-var ip      = require('ip');
+var url = require('url');
+var fs = require('fs');
+var dgram = require('dgram');
+var ip = require('ip');
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
@@ -18,64 +18,62 @@ var HOST = '127.0.0.1';
 
 var server = dgram.createSocket('udp4');
 
-server.on('listening', function () {
+server.on('listening', function() {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 
-server.on('message', function (message, remote) {
+server.on('message', function(message, remote) {
 
     var buf = Buffer.from(message);
     var body = null;
-    var type = buf.readInt8(0,0);
+    var type = buf.readInt8(0, 0);
 
-    if(type == 0){
-      buf.writeInt8(1,0);
-	  // timeout
-      server.send(buf, 3000, '127.0.0.1', function(err, bytes) {
-          if (err) throw err;
-          console.log('UDP SYN-ACK message sent to ' + HOST +':'+ 3000);
-      });
-    }
+    if (type == 0) {
+        buf.writeInt8(1, 0);
+        // timeout
+        server.send(buf, 3000, '127.0.0.1', function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP SYN-ACK message sent to ' + HOST + ':' + 3000);
+        });
+    } else if (type == 2) {
+        body = JSON.parse(buf.toString('utf8', 11));
 
-    else if(type == 2){
-      body = JSON.parse(buf.toString('utf8', 11));
+        //GET Functions
+        if (body != null && body.type == "get") {
 
-      //GET Functions
-      if(body != null && body.type == "get"){
+            var output = {};
+            var parseUrl = body["tempUrl"].replace(/'/g, "");
 
-        var output = {};
-        var parseUrl = body["tempUrl"].replace(/'/g,"");
+            getStatus(parseUrl, function(StatusLog) {
+                output["status"] = StatusLog;
+                getBody(parseUrl, function(body) {
+                    output["body"] = body;
+                    getJsonFromUrl(parseUrl, function(args) {
+                        output["args"] = args;
+                        getHeaders(parseUrl, function(headers) {
+                            output["headers"] = headers;
+                            const length = Buffer.byteLength(JSON.stringify(output), 'utf8')
+                            const buffer2 = Buffer.allocUnsafe(length)
+                            const newBuffer = Buffer.concat([buf, buffer2], length + 11);
 
-        getStatus(parseUrl, function(StatusLog){
-          output["status"] = StatusLog;
-          getBody(parseUrl, function(body){
-            output["body"] = body;
-            getJsonFromUrl(parseUrl, function(args){
-              output["args"] = args;
-              getHeaders(parseUrl, function(headers){
-                output["headers"] = headers;
-                  const length = Buffer.byteLength(JSON.stringify(output), 'utf8')
-                  const buffer2 = Buffer.allocUnsafe(length)
-                  const newBuffer = Buffer.concat([buf, buffer2], length+11);
-
-                  newBuffer.writeInt8(4,0);
-                  newBuffer.write(JSON.stringify(output), 11);
-				  // timeout
-                  server.send(newBuffer, 3000, '127.0.0.1', function(err, bytes) {
-                      if (err) throw err;
-                      console.log('UDP message sent to ' + HOST +':'+ 3000);
-                  });
-              })
+                            newBuffer.writeInt8(4, 0);
+                            newBuffer.write(JSON.stringify(output), 11);
+                            // timeout
+                            server.send(newBuffer, 3000, '127.0.0.1', function(err, bytes) {
+                                if (err) throw err;
+                                console.log('UDP message sent to ' + HOST + ':' + 3000);
+                            });
+                        })
+                    })
+                })
             })
-          })
-        })
-      }
+        }
 
-      //POST FUNCTIONS
-      if(body != null && body.type == "post"){
+        //POST FUNCTIONS
+        if (body != null && body.type == "post") {
 
-      }
+        }
 
     }
 
@@ -83,8 +81,8 @@ server.on('message', function (message, remote) {
 
 });
 
-server.on('data', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
+server.on('data', function(message, remote) {
+    console.log(remote.address + ':' + remote.port + ' - ' + message);
 
 });
 
@@ -93,122 +91,122 @@ server.bind(PORT, HOST);
 
 //Get arguments
 function getJsonFromUrl(theUrl, callback) {
-  var queryString = theUrl.substring( theUrl.indexOf('?') + 1 );
-  var splits = queryString.split("&");
-  var args = {};
-  splits.forEach(function(pair){
-    var innerSplits = pair.split("=")
-    args[innerSplits[0]] = innerSplits[1];
-  });
-  callback(args);
+    var queryString = theUrl.substring(theUrl.indexOf('?') + 1);
+    var splits = queryString.split("&");
+    var args = {};
+    splits.forEach(function(pair) {
+        var innerSplits = pair.split("=")
+        args[innerSplits[0]] = innerSplits[1];
+    });
+    callback(args);
 }
 
 //Standard GET protocol
-function getProtocol(theUrl, callback){
-	request({
-	  url: theUrl,
-	  method: "GET",
-	  timeout: 10000,
-	  followRedirect: true,
-	  maxRedirects: 10
-	}, function(error, response, body) {
-	  callback(response.url);
-	});
+function getProtocol(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, function(error, response, body) {
+        callback(response.url);
+    });
 }
 
 //Standard POST protocol
-function postProtocol(theUrl, callback){
-	request({
-		url: theUrl,
-		method: "POST",
-		form: {
-		name: "Test"
-		}
-	}, function(error, response, body) {
-    callback(response.url);
-	});
+function postProtocol(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "POST",
+        form: {
+            name: "Test"
+        }
+    }, function(error, response, body) {
+        callback(response.url);
+    });
 }
 
 
 //Standard GET body
-function getBody(theUrl, callback){
-	request({
-	  url: theUrl,
-	  method: "GET"
-	}, function(error, response, body) {
-	  callback(body);
-	});
+function getBody(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "GET"
+    }, function(error, response, body) {
+        callback(body);
+    });
 }
 
 //Standard POST body
-function postBody(theUrl, callback){
-	request({
-		url: theUrl,
-		method: "POST",
-    form: {
-		    name: "Test"
-		}
-	}, function(error, response, body) {
-    callback(body);
-	});
+function postBody(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "POST",
+        form: {
+            name: "Test"
+        }
+    }, function(error, response, body) {
+        callback(body);
+    });
 }
 
 // part of -v
 //Standard GET status
-function getStatus(theUrl, callback){
-	request({
-	  url: theUrl,
-	  method: "GET",
-	  timeout: 10000,
-	  followRedirect: true,
-	  maxRedirects: 10
-	}, function(error, response, body) {
-	    callback(response.statusCode);
-	});
+function getStatus(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, function(error, response, body) {
+        callback(response.statusCode);
+    });
 }
 
 // part of -v
 //Standard POST status
-function postStatus(theUrl, callback){
-	request({
-		url: theUrl,
-		method: "POST"
-	}, function(error, response, body) {
-    callback(response.statusCode);
-	});
+function postStatus(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "POST"
+    }, function(error, response, body) {
+        callback(response.statusCode);
+    });
 }
 
 // -h and -v
 //Standard GET headers
-function getHeaders(theUrl, callback){
-	request({
-	  url: theUrl,
-	  method: "GET",
-	  timeout: 10000,
-	  followRedirect: true,
-	  maxRedirects: 10
-	}, function(error, response, body) {
-	  callback(response.headers);
-	});
+function getHeaders(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, function(error, response, body) {
+        callback(response.headers);
+    });
 }
 
 // -h and -v
 //Standard POST headers
-function postHeaders(theUrl, callback){
-	request({
-		url: theUrl,
-		method: "POST",
-    form: {
-		name: "Test"
-		}
-	}, function(error, response, body) {
-    callback(response.headers);
-	});
+function postHeaders(theUrl, callback) {
+    request({
+        url: theUrl,
+        method: "POST",
+        form: {
+            name: "Test"
+        }
+    }, function(error, response, body) {
+        callback(response.headers);
+    });
 }
 
-function writeToFile(filename, towrite){
-  var wstream = fs.createWriteStream(String(filename));
-  wstream.write(towrite);
-  wstream.end();
+function writeToFile(filename, towrite) {
+    var wstream = fs.createWriteStream(String(filename));
+    wstream.write(towrite);
+    wstream.end();
 
 }
